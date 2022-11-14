4aecd07d695a2bc29796b8ca74873dbff0e87834
feifei <2388433587@qq.com>
2022-11-14 16:40:12 +0800

    hot-Fix2 commit

--- src/main/java/com/ff/git/GitTest.java
+++ src/main/java/com/ff/git/GitTest.java
@@ -6,7 +6,7 @@ public class GitTest {
         System.out.println("hello Git!");
         System.out.println("hello Git2!");
         System.out.println("hello git3");
-        System.out.println("SB");
+        System.out.println("hot-fix");
     }
 
 }
